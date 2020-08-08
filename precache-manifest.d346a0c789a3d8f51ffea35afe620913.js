99d4389e3e837b3d83e1b81f1fc3704d68bd97f8
KaoYuChunG <kaoyuchun14@gmail.com>
2020-08-08 12:18:44 -0300

    Updates

---
+++ precache-manifest.d346a0c789a3d8f51ffea35afe620913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfbc6c7ccee4fe084233fa65912f10c",
+    "url": "/medicina/index.html"
+  },
+  {
+    "revision": "93d28f59ab979f26e4d8",
+    "url": "/medicina/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c7eb42b85af17ac205c",
+    "url": "/medicina/static/js/2.57f52ad3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/medicina/static/js/2.57f52ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d28f59ab979f26e4d8",
+    "url": "/medicina/static/js/main.f0caa463.chunk.js"
+  },
+  {
+    "revision": "b6f80e385ffe6d3f36e5",
+    "url": "/medicina/static/js/runtime-main.3a638d3c.js"
+  }
+]);
